098db5b7d147b9ec1d7d00d8f591b33625cc0ea8
Fahmi Affandy <affandifahmi@gmail>
2025-11-17 20:47:15 +0700

    Menambahkan _api dan _services

---
+++ frontend/src/_services/books.js
@@ -0,0 +1,46 @@
+import { API } from "../_api/index"
+
+export const getBooks = async () => {
+    const { data } = await API.get("/books")
+    return data.data;
+};
+
+export const createBook = async (data) => {
+    try {
+        const response = await API.post("/books", data)
+        return response.data
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
+
+export const showBook = async (id) => {
+    try {
+        const { data } = await API.get(`/books/${id}`)
+        return data.data
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
+
+export const updateBook = async (id, data) => {
+    try {
+        const response = await API.post(`/books/${id}`, data)
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
+
+export const deleteBook = async (id) => {
+    try {
+        const response = await API.delete(`/books/${id}`, {
+            _method: "DELETE"
+        })
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
